c9b56c9ab789faa37b895c21bab4a4cfed983b6f
Sagar Chaudhary <126312151+sagar-my@users.noreply.github.com>
2025-10-28 13:08:42 +0530

    Update CartPage.java

--- src/test/java/com/saucedemo/automation_test/pages/CartPage.java
+++ src/test/java/com/saucedemo/automation_test/pages/CartPage.java
@@ -7,18 +7,26 @@ import org.testng.Assert;
 public class CartPage {
     private WebDriver driver;
 
-    private By itemName = By.className("inventory_item_name");
+   // private By itemName = By.className("inventory_item_name");
     private By itemPrice = By.className("inventory_item_price");
-
+private By cartItems = By.className("inventory_item_name");
     public CartPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void verifyCartDetails() {
-        String name = driver.findElement(itemName).getText();
+        String name = driver.findElement(cartItems).getText();
         String price = driver.findElement(itemPrice).getText();
 
         Assert.assertEquals(name, "Sauce Labs Backpack", "Item name mismatch!");
         Assert.assertEquals(price, "$29.99", "Item price mismatch!");
+        
+        public boolean isItemInCart(String itemName) {
+        List<String> items = driver.findElements(cartItems)
+                .stream()
+                .map(e -> e.getText().trim())
+                .collect(Collectors.toList());
+
+        return items.contains(itemName);
     }
 }
