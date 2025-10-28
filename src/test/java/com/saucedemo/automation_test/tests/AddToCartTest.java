1f97337228ef9b4b3e59187b70b4cae9b6dd094c
Sagar Chaudhary <126312151+sagar-my@users.noreply.github.com>
2025-10-28 13:02:21 +0530

    Update AddToCartTest.java

--- src/test/java/com/saucedemo/automation_test/tests/AddToCartTest.java
+++ src/test/java/com/saucedemo/automation_test/tests/AddToCartTest.java
@@ -5,18 +5,22 @@ import com.saucedemo.automation_test.pages.LoginPage;
 import com.saucedemo.automation_test.pages.InventoryPage;
 import com.saucedemo.automation_test.pages.CartPage;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class AddToCartTest extends BaseTest {
 
     @Test
-    public void testAddToCart() {
+    public void verifyAddMultipleItemsToCart() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         InventoryPage inventoryPage = new InventoryPage(driver);
         CartPage cartPage = new CartPage(driver);
-
         loginPage.login("standard_user", "secret_sauce");
-        inventoryPage.addBackpackToCart();
+        inventoryPage.addItemToCart("Sauce Labs Backpack");
+        Thread.sleep(2000); 
+        inventoryPage.addItemToCart("Sauce Labs Bike Light");
+        Thread.sleep(2000);
         inventoryPage.goToCart();
-        cartPage.verifyCartDetails();
+        Assert.assertTrue(cartPage.isItemInCart("Sauce Labs Backpack"), "Backpack not found in cart!");
+        Assert.assertTrue(cartPage.isItemInCart("Sauce Labs Bike Light"), "Bike Light not found in cart!");
     }
 }
